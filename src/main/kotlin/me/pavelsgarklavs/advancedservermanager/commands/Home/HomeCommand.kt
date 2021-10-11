99de933465829d885d357bd1a9629514d446479d
Pāvels Garklāvs <pavelsgarklavs@gmail.com>
2021-10-11 10:18:20 +0300

    Home Command

--- src/main/kotlin/me/pavelsgarklavs/advancedservermanager/commands/Home/HomeCommand.kt
+++ src/main/kotlin/me/pavelsgarklavs/advancedservermanager/commands/Home/HomeCommand.kt
@@ -1,6 +1,5 @@
 package me.pavelsgarklavs.advancedservermanager.commands.Home
 
-import com.google.common.io.Resources.getResource
 import me.pavelsgarklavs.advancedservermanager.AdvancedServerManager
 import me.pavelsgarklavs.advancedservermanager.utilities.Utils
 import org.bukkit.Bukkit
@@ -8,52 +7,76 @@ import org.bukkit.Location
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
 import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.configuration.file.YamlConfiguration
 import org.bukkit.entity.Player
 import java.io.File
 
 
-class HomeCommand(plugin: AdvancedServerManager): CommandExecutor, Utils(plugin) {
-
+class HomeCommand(plugin: AdvancedServerManager) : CommandExecutor, TabCompleter, Utils(plugin) {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-
         if (args.isEmpty()) {
             ifPermissible(sender, "advancedservermanager.home", {
                 val player: Player = sender as Player
-                val homes: FileConfiguration = YamlConfiguration.loadConfiguration(File(plugin.dataFolder, "homes.yml"))
+                try {
+                    val homes: FileConfiguration =
+                        YamlConfiguration.loadConfiguration(File(plugin.dataFolder, "homes.yml"))
 
-                val x = homes.get(player.name + ".x")
-                val y = homes.get(player.name + ".y")
-                val z = homes.get(player.name + ".z")
-                val playerWorld = homes.get(player.name + ".world")
+                    val x = homes.get(player.name + ".x")
+                    val y = homes.get(player.name + ".y")
+                    val z = homes.get(player.name + ".z")
+                    val playerWorld = homes.get(player.name + ".world")
 
-                val world = Bukkit.getServer().getWorld(playerWorld.toString())
+                    val world = Bukkit.getServer().getWorld(playerWorld.toString())
+                    val loc = Location(world, (x as Int).toDouble(), (y as Int).toDouble(), (z as Int).toDouble())
 
-                val loc = Location(world, (x as Int).toDouble(), (y as Int).toDouble(), (z as Int).toDouble())
-
-                player.teleport(loc)
-                sender.sendMessage(getConfigMessage("GoHome"))
+                    player.teleport(loc)
+                    sender.sendMessage(getConfigMessage("GoHome"))
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    sender.sendMessage(getConfigMessage("SomethingWentWrong"))
+                }
             }, true)
             return true
         } else if (args.size == 1) {
             ifPermissible(sender, "advancedservermanager.home.players", {
                 val player: Player = sender as Player
-                val homes: FileConfiguration = YamlConfiguration.loadConfiguration(File(plugin.dataFolder, "homes.yml"))
+                try {
+                    val homes: FileConfiguration =
+                        YamlConfiguration.loadConfiguration(File(plugin.dataFolder, "homes.yml"))
 
-                val x = homes.get(args[0] + ".x")
-                val y = homes.get(args[0] + ".y")
-                val z = homes.get(args[0] + ".z")
-                val playerWorld = homes.get(args[0] + ".world")
+                    val x = homes.get(args[0] + ".x")
+                    val y = homes.get(args[0] + ".y")
+                    val z = homes.get(args[0] + ".z")
+                    val playerWorld = homes.get(args[0] + ".world")
 
-                val world = Bukkit.getServer().getWorld(playerWorld.toString())
+                    val world = Bukkit.getServer().getWorld(playerWorld.toString())
+                    val loc = Location(world, (x as Int).toDouble(), (y as Int).toDouble(), (z as Int).toDouble())
 
-                val loc = Location(world, (x as Int).toDouble(), (y as Int).toDouble(), (z as Int).toDouble())
-
-                player.teleport(loc)
-                sender.sendMessage(getConfigMessage("GoHome"))
+                    player.teleport(loc)
+                    sender.sendMessage(getConfigMessage("GoHome"))
+                } catch (e: NullPointerException) {
+                    sender.sendMessage(getConfigMessage("HomeDoesNotExist"))
+                }
             }, true)
+            return true
         }
         return true
+    }
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        alias: String,
+        args: Array<out String>
+    ): MutableList<String> {
+        val completions: MutableList<String> = mutableListOf()
+        if (args.size == 1) {
+            for (OfflinePlayer in Bukkit.getOfflinePlayers()) {
+                OfflinePlayer.name?.let { completions.add(it) }
+            }
+        }
+        return completions
     }
 }
--- src/main/kotlin/me/pavelsgarklavs/advancedservermanager/commands/Home/SetHomeCommand.kt
+++ src/main/kotlin/me/pavelsgarklavs/advancedservermanager/commands/Home/SetHomeCommand.kt
@@ -10,23 +10,17 @@ import org.bukkit.entity.Player
 import java.io.File
 
 
-class SetHomeCommand(plugin: AdvancedServerManager): CommandExecutor, Utils(plugin) {
-
-    private val folder: File = plugin.dataFolder
-
+class SetHomeCommand(plugin: AdvancedServerManager) : CommandExecutor, Utils(plugin) {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-
-        if(args.isEmpty()) {
+        if (args.isEmpty()) {
             ifPermissible(sender, "advancedservermanager.sethome", {
                 val player: Player = sender as Player
-
                 try {
                     val file = File(plugin.dataFolder, "homes.yml")
                     val config = YamlConfiguration()
                     if (!file.exists()) {
                         file.createNewFile()
                     }
-
                     config.set(player.name + ".x", player.location.blockX)
                     config.set(player.name + ".y", player.location.blockY)
                     config.set(player.name + ".z", player.location.blockZ)
@@ -34,9 +28,9 @@ class SetHomeCommand(plugin: AdvancedServerManager): CommandExecutor, Utils(plug
 
                     config.save(file)
                     sender.sendMessage(getConfigMessage("SetHome"))
-
-                } catch (ex: Exception) {
-                    ex.printStackTrace()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    sender.sendMessage(getConfigMessage("SomethingWentWrong"))
                 }
             }, true)
             return true
