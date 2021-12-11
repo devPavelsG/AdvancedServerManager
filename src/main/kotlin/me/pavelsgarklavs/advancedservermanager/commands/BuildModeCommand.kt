3a84eaaa4c11de262682c57a8d84148e435af481
Pāvels <pavelsgarklavs@gmail.com>
2021-12-11 15:45:54 +0200

    Build Mode functionality added to the plugin.

---
+++ src/main/kotlin/me/pavelsgarklavs/advancedservermanager/commands/BuildModeCommand.kt
@@ -0,0 +1,54 @@
+package me.pavelsgarklavs.advancedservermanager.commands
+
+import me.pavelsgarklavs.advancedservermanager.AdvancedServerManager
+import me.pavelsgarklavs.advancedservermanager.utilities.Utils
+import org.bukkit.GameMode
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.entity.Player
+import java.io.File
+
+class BuildModeCommand(plugin: AdvancedServerManager) : CommandExecutor, Utils(plugin) {
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (args.isEmpty()) {
+            ifPermissible(sender, "advancedservermanager.buildmode", {
+                val player: Player = sender as Player
+                try {
+                    val playerUUID = player.uniqueId.toString()
+                    val file = File(plugin.dataFolder, "players.yml")
+                    if (!file.exists()) {
+                        file.createNewFile()
+                    }
+                    val config = YamlConfiguration.loadConfiguration(file)
+                    if (!config.contains(playerUUID)) {
+                        config.set("$playerUUID.buildmode", true)
+                    }
+
+                    val isInBuildMode = config.get("$playerUUID.buildmode") as Boolean
+
+                    if (isInBuildMode && player.gameMode == GameMode.CREATIVE) {
+                        player.sendMessage(getConfigMessage("BuildModeOff"))
+                        config.set("$playerUUID.buildmode", false)
+                    } else if (!isInBuildMode && player.gameMode == GameMode.CREATIVE) {
+                        player.sendMessage(getConfigMessage("BuildModeOn"))
+                        config.set("$playerUUID.buildmode", true)
+                    } else {
+                        player.sendMessage(getConfigMessage("BuildModeWhenNotCreative"))
+                    }
+                    config.save(file)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    sender.sendMessage(getConfigMessage("SomethingWentWrong"))
+                }
+            }, true)
+            return true
+        } else {
+            ifPermissible(sender, "advancedservermanager.buildmode", {
+                sender.sendMessage(getConfigMessage("ErrorArguments"))
+            })
+            return false
+        }
+    }
+}
---
+++ src/main/kotlin/me/pavelsgarklavs/advancedservermanager/listeners/BuildModeListener.kt
@@ -0,0 +1,68 @@
+package me.pavelsgarklavs.advancedservermanager.listeners
+
+import me.pavelsgarklavs.advancedservermanager.AdvancedServerManager
+import me.pavelsgarklavs.advancedservermanager.utilities.Utils
+import org.bukkit.GameMode
+import org.bukkit.configuration.file.YamlConfiguration
+import org.bukkit.event.Event
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.Action
+import org.bukkit.event.player.PlayerGameModeChangeEvent
+import org.bukkit.event.player.PlayerInteractEvent
+import java.io.File
+
+class BuildModeListener(plugin: AdvancedServerManager) : Listener, Utils(plugin) {
+
+    @EventHandler
+    fun breakBlockEvent(breakEvent: PlayerInteractEvent) {
+        val player = breakEvent.player
+        if (breakEvent.action == Action.LEFT_CLICK_BLOCK && player.gameMode == GameMode.CREATIVE) {
+            try {
+                val playerUUID = player.uniqueId.toString()
+                val file = File(plugin.dataFolder, "players.yml")
+                if (!file.exists()) {
+                    file.createNewFile()
+                }
+                val config = YamlConfiguration.loadConfiguration(file)
+                if (!config.contains(playerUUID)) {
+                    config.set("$playerUUID.buildmode", true)
+                }
+
+                val isInBuildMode = config.get("$playerUUID.buildmode") as Boolean
+                config.save(file)
+
+                if (isInBuildMode) {
+                    player.sendMessage(getConfigMessage("BuildModeWhenOn"))
+                }
+
+                if (isInBuildMode) {
+                    breakEvent.setUseInteractedBlock(Event.Result.DENY)
+                } else {
+                    breakEvent.setUseInteractedBlock(Event.Result.ALLOW)
+                }
+            } catch (e: Exception) {
+                println(e.printStackTrace())
+            }
+        }
+    }
+
+    @EventHandler
+    fun gameModeSwitchEvent(switchEvent: PlayerGameModeChangeEvent) {
+        val player = switchEvent.player
+        try {
+            val playerUUID = player.uniqueId.toString()
+            val file = File(plugin.dataFolder, "players.yml")
+            if (!file.exists()) {
+                file.createNewFile()
+            }
+            val config = YamlConfiguration.loadConfiguration(file)
+            if (switchEvent.newGameMode == GameMode.CREATIVE) {
+                config.set("$playerUUID.buildmode", true)
+            }
+            config.save(file)
+        } catch (e: Exception) {
+            println(e.printStackTrace())
+        }
+    }
+}
